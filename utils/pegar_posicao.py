013aa1ed8883be8efae055b3d513b0586fa13f57
dalmeidasdev <dalmeidasdev@gmail.com>
2026-09-11 21:21:51 -0300

    Correção função

--- utils/pegar_posicao.py
+++ utils/pegar_posicao.py
@@ -2,11 +2,11 @@
 
 import pyautogui as pa
 
-def pegarposicao():
+def pegar_posicao():
     print('=' * 100)
     print('Bem vindo, abaixo está a posição atual do seu mouse, quando achar a posição que precisar aperte CTRL + C\n')
     print('Dica: se após o CRTL + C ainda continuar mudando a posição clique no terminal uma vez e volte para a posição desejada e tente denovo')
     print('=' * 100)
-    print(pa.displayMousePosition())
+    pa.displayMousePosition()
 
-pegarposicao()
+pegar_posicao()
